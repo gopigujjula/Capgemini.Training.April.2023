9d15961645d45e103dd319e3c86942889a74e2b8
user <user@DESKTOP-K0A4BQO>
2023-04-26 17:16:33 +0530

    Add project files.

---
+++ Brainwave.Project/Controllers/HeroController.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gymster.Project.Controllers
+{
+    public class HeroController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dataSourceItem = RenderingContext.Current.Rendering.Item;
+            return View("~/Views/Gymster/Components/HeroControllerExample.cshtml", dataSourceItem);
+        }
+    }
+}
